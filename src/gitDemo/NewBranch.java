6407f6bb6cefa46c5042ecbf5ae10bf2e6229b78
yassharm <yassharm@adobe.com>
2023-07-02 14:56:27 +0530

    student branch, not master

---
+++ src/gitDemo/NewBranch.java
@@ -0,0 +1,10 @@
+package gitDemo;
+
+public class NewBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Student branch added");
+	}
+
+}
